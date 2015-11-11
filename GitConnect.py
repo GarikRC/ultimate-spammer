0ed4c7b3730739033b0066f1f4e75d9b6bb178c2
qubasa <qubasa.qube@gmail.com>
2015-11-11 18:16:44 +0100

    Updated version.

--- GitConnect.py
+++ GitConnect.py
@@ -3,25 +3,23 @@ import hashlib
 import requests
 import platform
 import os
-import re
+import time
 
 
 __author__ = 'qubasa'
 
-# Variables
-if platform.system() == 'Windows':
-    pathsign = '\\'
-else:
-    pathsign = '/'
 
+def CheckRepo(dirpath):
 
-def CheckRepo(path):
-
-    # https://api.github.com/repos/Qubasa/ultimate-spammer/contents/[filename]
+    # Method variables
+    downloadlink = 'https://raw.githubusercontent.com/Qubasa/ultimate-spammer/master/'  # [file]
     repolink = 'https://api.github.com/repos/Qubasa/ultimate-spammer/contents/'
-    files = os.listdir(path)
+    files = os.listdir(dirpath)
+    textfilename = "saveSHA.txt"
     sha1 = hashlib.sha1()
     allowed = ['py', '.md']
+    currentdate = time.strftime("%d/%m/%Y")
+    iterations = -1
 
     # Exclude unallowed file endings
     for currentfile in files:
@@ -29,29 +27,71 @@ def CheckRepo(path):
         if not currentfile.endswith(tuple(allowed)):
             files.remove(currentfile)
 
-    # Weird bug solution
-    if not files[0].endswith(tuple(allowed)):
-        files.remove(files[0])
+    # Repeat if not done some .pyc files remain (bug ?)
+    for currentfile in files:
+
+        if not currentfile.endswith(tuple(allowed)):
+            files.remove(currentfile)
+
+    print files
+
+    # Get date out of saveSHA file
+    saveSHA = open(textfilename, 'rb')     # Read file
+    if os.path.getsize(os.path.join(dirpath, textfilename)) > 0:   # If it is not empty
+
+        filedate = saveSHA.readline().split("|")[0].split(":")[2]   # Get date of first entry
+        print "Filedate: " + filedate + " Currentdate: " + currentdate
+
+    else:
+        filedate = None
+        print "No entries were detected"
+    saveSHA.close()
+
+    # Delete old text entries
+    if currentdate != filedate:
+        print "Delete old Entries"
+        saveSHA = open(textfilename, 'wb')
+        saveSHA.close()
 
     # Check SHA from files
     for currentfile in files:
 
-        # Get file SHA from Github
-        r = requests.get(repolink + currentfile)
-        if r.status_code is requests.codes.ok:
+        iterations = iterations + 1
 
-            datalist = r.text.split(',')
-            checksum = datalist[2].strip('"sha": "').strip('"')
+        # Get Github SHA and write to file
+        if currentdate != filedate:
 
+            # Get file SHA from Github
+            r = requests.get(repolink + currentfile)
+            if r.status_code is requests.codes.ok:
+
+                datalist = r.text.split(',')
+                checksum = datalist[2].strip('"sha": "').strip('"')
+                saveSHA = open(textfilename, 'ab')
+                saveSHA.write(currentfile + ":" + checksum + ":" + currentdate + "|")
+                saveSHA.close()
+                print "Download Sha"
+
+            else:
+                print "Connection error " + currentfile + " " + r.text
         else:
-            print "Connection error " + currentfile + " " + r.text
+            saveSHA = open(textfilename, 'rb')
+            checksum = saveSHA.readline().split("|")[iterations].split(":")[1]
+            saveSHA.close()
 
         # Calculate SHA from local file
         try:
-            f = open(path + pathsign + currentfile, 'rb')
-            sha1.update(f.read())  # Get SHA with sha1.hexdigest()
+            f = open(textfilename, 'rb')
+            #  Get SHA with sha1.hexdigest()
+            sha1.update("blob " + str(os.path.getsize(textfilename)) + f.read())
+
         finally:
             f.close()
 
         if sha1.hexdigest() is not checksum:
             print "[+] " + currentfile + " has to be updated"
+            print "Githubhash: " + checksum
+            print "Localhash:  " + sha1.hexdigest()
+            print
+
+    saveSHA.close()
