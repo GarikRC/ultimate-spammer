0bfd69e643b5552a5367af9df8a565abc59e5c5c
qubasa <qubasa.qube@gmail.com>
2015-11-14 00:54:32 +0100

    Updated version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ultimate-spammer',
     version='2.0',
-    packages=[''],
+    packages=['skype4py, selenium, requests, pyautogui, time, os, sys, hashlib'],
     url='https://github.com/Qubasa/ultimate-spammer',
     license='MIT License',
     author='Qubasa',
