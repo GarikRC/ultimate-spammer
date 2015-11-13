3a5e75f4da2ed7aedcc42eb8854d98f6fbd5ff86
qubasa <qubasa.qube@gmail.com>
2015-11-14 00:39:27 +0100

    Default 2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ultimate-spammer',
+    version='2.0',
+    packages=[''],
+    url='https://github.com/Qubasa/ultimate-spammer',
+    license='MIT License',
+    author='Qubasa',
+    author_email='',
+    description='A spammer for Whatsapp, Facebook, Skype and Email.'
+)
